45a829b0d95f659d41957e885a8ee837f7651d90
Emmanuel Tweneboah Koduah <emmanueltweneboah71@gmail.com>
2022-02-14 14:21:01 +0000

    created a slider component

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://cdn-images.farfetch-contents.com/14/93/51/56/14935156_25463153_1000.jpg",
+        title:" SUMMER SALE",
+        desc:" DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+      id:2,
+        img: "https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/a5/P00575142.jpg",
+        title: "WINTER SALE",
+        desc:  "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://cdn-images.farfetch-contents.com/14/93/51/56/14935156_25463153_1000.jpg" ,
+        title: "POPULAR SALE",
+        desc:" DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    }
+]
